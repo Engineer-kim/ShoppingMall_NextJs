0512a0693b7f33a193a6a55e0c4fd038d5f6c193
Kimhanjin <ddang0103@naver.com>
2025-05-28 18:44:19 +0900

    더미 데이터 화면에 뿌리기(Key Props 추가 꼭해야함)
    Key Prop 추가안하면 나중에 데이터가 식별이 안됨(물론 컴파일에는 문제가 없으나 , 크롬 브라우저의 개발자도구키면 키 Props 없다고 불평/불만이 뜸)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,14 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 
 const Homepage = () => {
-  return <>Store</>
+  //console.log("Sample Data:", sampleData);
+  return (
+    <>
+      <ProductList title='신상품요' data={sampleData.products} limit={4}/>
+    </>
+  )
 }
 
 export default Homepage;
