57bfd370141c189127c30444603661fde89949ca
Kimhanjin <ddang0103@naver.com>
2025-05-31 18:11:35 +0900

    유저 더미데이터 인서트
    비번은 bcrypt-ts-edge 통해서 bcrypt 해쉬 알고리즘(솔팅) 통해서 암호화되게끔
    해당 종속성도 --legacy-peer-deps 붙여서 설치함

--- db/seed.ts
+++ db/seed.ts
@@ -6,9 +6,16 @@ async function main() {
   const  prisma = new PrismaClient();
     // 기존 데이터 삭제
     await prisma.product.deleteMany({});
+    await prisma.user.deleteMany({});
+    await prisma.account.deleteMany({});
+    await prisma.session.deleteMany({});
+    await prisma.verificationToken.deleteMany({});
     //더미 데이터를 DB에 삽입
     await prisma.product.createMany({ data: sampleData.products });
-    console.log('데이터 SEED를 통해 생성 성공여:' + sampleData.products.length + '개');
+    await prisma.user.createMany({ data: sampleData.users });
+
+    console.log('데이터 SEED를 통해 생성 성공여:' + sampleData.products.length + '개(상품)');
+    console.log('데이터 SEED를 통해 생성 성공여:' + sampleData.users.length + '개(유저)');
 }
 
 main();
